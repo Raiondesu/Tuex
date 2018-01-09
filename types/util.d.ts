a99f70c71e0fba7ae1f581b7fd3de6eae82abde2
Raiondesu <a.raiondesu@gmail.com>
2018-01-09 21:59:34 +0300

    UPD.

--- types/util.d.ts
+++
@@ -1,8 +0,0 @@
-export declare const desc: (o: any, p: string) => PropertyDescriptor;
-export declare const keysOf: (o: any) => string[];
-export declare function isObject(obj: any): boolean;
-export declare function isFunction(fn: any): boolean;
-export declare function isPromise(value: any): boolean;
-export declare function isValue(descriptor: PropertyDescriptor): boolean;
-export declare function isGetter(descriptor: PropertyDescriptor): boolean;
-export declare function isSetter(descriptor: PropertyDescriptor): (v: any) => void;
